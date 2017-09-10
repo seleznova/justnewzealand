0d2391bd9e41f54dcaab84882ceb74b0a0912194
seleznova <mary.kuzmenko@gmail.com>
2017-09-10 17:59:16 +0100

    updated int

--- app/src/main/java/marynakuzmenko/justnewzealand/MainActivity.java
+++ app/src/main/java/marynakuzmenko/justnewzealand/MainActivity.java
@@ -29,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        int numberOfPeople = 2;
+        display(numberOfPeople);
+        displayPrice(numberOfPeople / 100);
     }
 
     /**
