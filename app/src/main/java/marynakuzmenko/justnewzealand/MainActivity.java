18bf2f3dbaa3dea9c06fee62ffeb54ddc334cec7
seleznova <mary.kuzmenko@gmail.com>
2017-09-10 17:33:41 +0100

    updates in two core files

--- app/src/main/java/marynakuzmenko/justnewzealand/MainActivity.java
+++ app/src/main/java/marynakuzmenko/justnewzealand/MainActivity.java
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import marynakuzmenko.justnewzealand.R;
+
 /**
  * This app displays an order form to order coffee.
  */
@@ -36,5 +38,12 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
